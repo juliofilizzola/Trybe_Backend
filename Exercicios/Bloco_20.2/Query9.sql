6306af8b05bfd3e304f7e3434d176692f778ebf6
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 19:16:06 -0300

    atividade 3 (true)

--- Exercicios/Bloco_20.2/Query9.sql
+++ Exercicios/Bloco_20.2/Query9.sql
@@ -2,3 +2,4 @@ USE Scientists;
 
 SELECT concat('This is SQL Exercise,', 'Practice and Solution') as 'frase' From Projects Limit 1;
 Select 1 as firt_column, 2 as second_column, 3 as thirth_column;
+select 10 + 15;
