b4518dcfb66c7cc563d345aff69268e693f03c2b
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 15:52:10 -0300

    comentario (## filmes, ano de lançamento e também a classificação)

--- Exercicios/Bloco_20.2/Query7.sql
+++ Exercicios/Bloco_20.2/Query7.sql
@@ -3,5 +3,5 @@ USE sakila;
 select * from film;
 ## todos os filmes
 select title, release_year, rating from film;
-
+## filmes, ano de lançamento e também a classificação
 select count(title) from film;
