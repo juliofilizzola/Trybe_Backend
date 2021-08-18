273e9d4170e163ddc4b39bc312b246a3d1d199bb
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 14:25:29 -0300

    resuloção da atividade de count

--- Exercicios/Bloco_20.2/Query4.sql
+++ Exercicios/Bloco_20.2/Query4.sql
@@ -1,2 +1,8 @@
 USE sakila;
-SELECT COUNT(*) FROM actor;
+
+SELECT count(password) FROM staff;
+## Quantas senhas tem (1)
+SELECT COUNT(first_name) FROM staff;
+## Quantas pessoas tem (2)
+SELECT COUNT(email) FROM staff;
+## Quantos emails tem (2)
