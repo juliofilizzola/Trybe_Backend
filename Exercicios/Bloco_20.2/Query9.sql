009e767a70ab9ee13ec39b9908f12a400b94cc20
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 20:07:38 -0300

    atividade 4 e algumas refaturações

--- Exercicios/Bloco_20.2/Query9.sql
+++ Exercicios/Bloco_20.2/Query9.sql
@@ -1,6 +1,9 @@
-USE Scientists;
 
 SELECT concat('This is SQL Exercise,', 'Practice and Solution');
 Select 1 as firt_column, 2 as second_column, 3 as thirth_column;
 select 10 + 15;
 select 10 * 5;
+
+USE Scientists;
+
+select name from Scientists;
