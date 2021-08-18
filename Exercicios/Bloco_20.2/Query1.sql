d64b5c1ff712a744d4988f9f8cc71c342ebd8327
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 10:15:21 -0300

    puxando Title e release year de films do sakila

--- Exercicios/Bloco_20.2/Query1.sql
+++ Exercicios/Bloco_20.2/Query1.sql
@@ -1,3 +1,4 @@
 USE sakila;
 SELECT title, description, release_year FROM film;
 SELECT CONCAT (first_name, ' ', last_name) AS 'Nome Completo' from actor;
+SELECT CONCAT (title, ' ', release_year) as 'Lançamento de Filme' FROM film;
