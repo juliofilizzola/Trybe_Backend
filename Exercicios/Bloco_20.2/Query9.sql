47a3883d2a20102f08ded3003329b39890c62e0e
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 18:54:39 -0300

    refaturação

--- Exercicios/Bloco_20.2/Query9.sql
+++ Exercicios/Bloco_20.2/Query9.sql
@@ -1,3 +1,3 @@
 USE Scientists;
 
-SELECT concat('This is SQL Exercise,', 'Practice and Solution') From Projects;
+SELECT concat('This is SQL Exercise,', 'Practice and Solution') as 'frase' From Projects Limit 1;
