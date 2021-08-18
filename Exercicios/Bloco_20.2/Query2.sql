0b66cfa307fe76e059957bafb6ac18dfb21edc73
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 11:17:59 -0300

    criando a banco de dados

--- Exercicios/Bloco_20.2/Query2.sql
+++ Exercicios/Bloco_20.2/Query2.sql
@@ -1,1 +1,12 @@
-USE sakila;
+CREATE DATABASE `Escola`;
+CREATE TABLE IF NOT EXISTS Escola.Alunos (
+    `Nome` VARCHAR(7) CHARACTER SET utf8,
+    `Idade` INT
+);
+INSERT INTO Escola.Alunos VALUES
+    ('Rafael', 25),
+    ('Amanda', 30),
+    ('Roberto', 45),
+    ('Carol', 19),
+    ('Amanda', 25);
+
