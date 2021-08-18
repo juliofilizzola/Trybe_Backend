e717955fd5474592052cdca030efa15f9ae43f05
Filizzola <juliofilizzola@hotmail.com>
2021-08-18 10:51:02 -0300

    mais um exemplo com concat

--- Exercicios/Bloco_20.2/Query1.sql
+++ Exercicios/Bloco_20.2/Query1.sql
@@ -3,3 +3,4 @@ SELECT title, description, release_year FROM film;
 SELECT CONCAT (first_name, ' ', last_name) AS 'Nome Completo' from actor;
 SELECT CONCAT (title, ' ', release_year) as 'Lançamento de Filme' FROM film;
 Select concat (title, ' ', rating) as 'Classificação' from film;
+select concat (address, '', district) as 'Endereço' from address;
