829566561c498d1e4ae7546dfb26a1f28622b440
Filizzola <juliofilizzola@hotmail.com>
2021-08-19 09:51:51 -0300

    bonus 1c

--- Exercicios/Bloco_20.2/Query12.sql
+++ Exercicios/Bloco_20.2/Query12.sql
@@ -2,3 +2,4 @@ use PiecesProviders;
 
 SELECT * FROM Provides WHERE PROVIDER = 'RBT';
 SELECT * FROM Provides ORDER BY Price DESC LIMIT 5;
+SELECT * FROM Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
