7a296b8155ea0da2aa3f2a99ab6367968690c580
Filizzola <juliofilizzola@hotmail.com>
2021-08-19 09:25:23 -0300

    atividade 15

--- Exercicios/Bloco_20.2/Query10.sql
+++ Exercicios/Bloco_20.2/Query10.sql
@@ -3,3 +3,4 @@ USE Scientists;
 SELECT Name FROM Projects ORDER BY Hours DESC LIMIT 1;
 SELECT Name FROM Projects ORDER BY Hours ASC LIMIT 1 OFFSET 1;
 SELECT * FROM Projects ORDER BY Hours ASC LIMIT 5;
+SELECT CONCAT('Existe ', COUNT(Name), ' cientistas na tablea Scientists.') FROM Scientists;
