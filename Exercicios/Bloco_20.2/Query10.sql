314b48a3f28d48cb3f6d489d73da0b73404036ae
Filizzola <juliofilizzola@hotmail.com>
2021-08-19 09:16:09 -0300

    atividade 14

--- Exercicios/Bloco_20.2/Query10.sql
+++ Exercicios/Bloco_20.2/Query10.sql
@@ -2,3 +2,4 @@ USE Scientists;
 
 SELECT Name FROM Projects ORDER BY Hours DESC LIMIT 1;
 SELECT Name FROM Projects ORDER BY Hours ASC LIMIT 1 OFFSET 1;
+SELECT * FROM Projects ORDER BY Hours ASC LIMIT 5;
