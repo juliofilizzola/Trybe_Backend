e0bf903b6ac38227f5e474b42943e44495c3c9b8
Filizzola <juliofilizzola@hotmail.com>
2021-08-20 09:41:30 -0300

    atividade 3

--- Exercicios/Bloco_20.4/Query6.sql
+++ Exercicios/Bloco_20.4/Query6.sql
@@ -12,3 +12,8 @@ SELECT * FROM BoxOffice;
 INSERT INTO BoxOffice (movie_id, rating, domestic_sales, international_sales)
 	VALUES(9, 6.8, 450000000, 370000000);
 SELECT * FROM BoxOffice;
+
+UPDATE Movies
+SET director = 'Andrew Staton'
+WHERE director = 'John Lasseter';
+SELECT * FROM Movies;
