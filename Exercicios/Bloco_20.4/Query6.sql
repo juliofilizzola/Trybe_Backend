89f93ef4ad1f53ea36531470dc1ad56c727a0472
Filizzola <juliofilizzola@hotmail.com>
2021-08-20 09:55:15 -0300

    atividade 5

--- Exercicios/Bloco_20.4/Query6.sql
+++ Exercicios/Bloco_20.4/Query6.sql
@@ -24,3 +24,10 @@ WHERE title = 'ratatui';
 
 SELECT * FROM Movies;
 
+INSERT INTO BoxOffice (movie_id, rating, domestic_sales, international_sales)
+	VALUES(8, 8.5, 300000000, 250000000),
+		(10, 7.4, 460000000, 510000000),
+        (11, 9.9, 290000000, 280000000);
+        
+SELECT * FROM BoxOffice;
+        
