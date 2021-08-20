a49c5196d4215afaa9a85605e5cf0684cfe8fa5e
Filizzola <juliofilizzola@hotmail.com>
2021-08-20 09:25:05 -0300

    Atividade 1

--- Exercicios/Bloco_20.4/Query6.sql
+++ Exercicios/Bloco_20.4/Query6.sql
@@ -1,1 +1,9 @@
 USE Pixar;
+
+INSERT INTO Movies(title, director, year, length_minutes)
+  VALUES ('Monstros SA', 'Pete Docter', 2001, 92),
+		('Procurando Nemo', 'John Lasset', 2003, 107),
+        ('Os Incriveis', 'Brad Bird', 2004, 116),
+        ('WALL-E',' Pete Docter', 2008, 104);
+        
+SELECT * FROM Movies;
