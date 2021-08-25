82f6a568d90269856612af1c502d65ce471eed50
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 15:45:58 -0300

    exemplo 3

--- Exercicios/Bloco_21.2/Query4.sql
+++ Exercicios/Bloco_21.2/Query4.sql
@@ -7,3 +7,11 @@ UNION ALL
 (SELECT first_name FROM customer WHERE first_name LIKE '%tracy%')
 UNION
 (SELECT first_name FROM actor WHERE first_name LIKE '%je%');
+-- exemplo 3
+
+(SELECT first_name FROM actor ORDER BY actor_id DESC LIMIT 5)
+UNION
+(SELECT first_name FROM staff LIMIT 1)
+UNION
+(SELECT first_name FROM customer LIMIT 5 OFFSET 15)
+ORDER BY first_name;
