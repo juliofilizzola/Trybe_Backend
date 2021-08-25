2365f17ce439fe98089e7f929c529676fe714730
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 15:41:11 -0300

    exemplo 2

--- Exercicios/Bloco_21.2/Query4.sql
+++ Exercicios/Bloco_21.2/Query4.sql
@@ -1,5 +1,9 @@
 USE sakila;
-
+-- exemplo 1
 (SELECT first_name, last_name FROM staff)
 UNION ALL
 (SELECT first_name, last_name FROM actor);
+-- exemplo 2
+(SELECT first_name FROM customer WHERE first_name LIKE '%tracy%')
+UNION
+(SELECT first_name FROM actor WHERE first_name LIKE '%je%');
