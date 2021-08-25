2a486ae51d919dc9fc1afe8fe8129ded1b585e04
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 13:26:45 -0300

    exemplo 3

--- Exercicios/Bloco_21.2/Query2.sql
+++ Exercicios/Bloco_21.2/Query2.sql
@@ -1,4 +1,9 @@
 USE sakila;
-select * from staff;
+-- exemplo 1
 SELECT a.actor_id, a.first_name, f.film_id FROM actor AS a INNER JOIN film_actor AS f ON a.actor_id = f.actor_id;
+-- exemplo 2
 SELECT s.first_name, s.last_name, a.address FROM staff AS s INNER JOIN address AS a ON s.address_id = a.address_id;
+-- exemplo 3
+SELECT c.customer_id, c.first_name, c.email, a.address_id, a.address FROM customer AS c 
+	INNER JOIN address AS a ON c.address_id = a.address_id;
+
