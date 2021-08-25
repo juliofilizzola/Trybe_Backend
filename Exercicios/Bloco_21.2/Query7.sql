100c671a24ea0678c7f9eaccd8b26f3a17a6671a
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 20:21:03 -0300

    atividade 2

--- Exercicios/Bloco_21.2/Query7.sql
+++ Exercicios/Bloco_21.2/Query7.sql
@@ -3,3 +3,6 @@ SELECT * FROM Movies;
 -- atividade 1 
 SELECT m.title,  b.domestic_sales, b.international_sales FROM Movies AS m
 	INNER JOIN BoxOffice AS b ON m.id = b.movie_id;
+-- atividade 2
+SELECT m.title,  b.domestic_sales, b.international_sales FROM Movies AS m
+	INNER JOIN BoxOffice AS b ON m.id = b.movie_id WHERE b.international_sales > b.domestic_sales;
