a166deebbad310ce847d1edeff556134a592a643
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 13:29:29 -0300

    finalizando exmeplo3

--- Exercicios/Bloco_21.2/Query2.sql
+++ Exercicios/Bloco_21.2/Query2.sql
@@ -5,5 +5,5 @@ SELECT a.actor_id, a.first_name, f.film_id FROM actor AS a INNER JOIN film_actor
 SELECT s.first_name, s.last_name, a.address FROM staff AS s INNER JOIN address AS a ON s.address_id = a.address_id;
 -- exemplo 3
 SELECT c.customer_id, c.first_name, c.email, a.address_id, a.address FROM customer AS c 
-	INNER JOIN address AS a ON c.address_id = a.address_id;
-
+	INNER JOIN address AS a ON c.address_id = a.address_id LIMIT 100;
+-- exemplo 4
