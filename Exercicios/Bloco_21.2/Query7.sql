c1c6d8c9c6d3d098c9168611856766a9c693dc42
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 20:37:47 -0300

    atividade 5

--- Exercicios/Bloco_21.2/Query7.sql
+++ Exercicios/Bloco_21.2/Query7.sql
@@ -7,4 +7,8 @@ SELECT m.title,  b.domestic_sales, b.international_sales FROM Movies AS m
 	INNER JOIN BoxOffice AS b ON m.id = b.movie_id WHERE b.international_sales > b.domestic_sales;
 -- atividade 3 
 SELECT m.title, b.rating FROM Movies AS m INNER JOIN BoxOffice AS b ON m.id = b.movie_id ORDER BY b.rating DESC;
+-- atividade 4
+SELECT t.name, t.location, m.title, m.director, m.year, m.length_minutes FROM Theater t 
+	LEFT JOIN Movies AS m ON t.id = m.theater_id ORDER BY t.name;
+
     
