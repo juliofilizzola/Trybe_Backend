75ea9b2797538c6137c2ec1bcc91d39052890611
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 20:26:30 -0300

    atividade 3

--- Exercicios/Bloco_21.2/Query7.sql
+++ Exercicios/Bloco_21.2/Query7.sql
@@ -1,8 +1,10 @@
 USE Pixar;
-SELECT * FROM Movies;
 -- atividade 1 
 SELECT m.title,  b.domestic_sales, b.international_sales FROM Movies AS m
 	INNER JOIN BoxOffice AS b ON m.id = b.movie_id;
 -- atividade 2
 SELECT m.title,  b.domestic_sales, b.international_sales FROM Movies AS m
 	INNER JOIN BoxOffice AS b ON m.id = b.movie_id WHERE b.international_sales > b.domestic_sales;
+-- atividade 3 
+SELECT m.title, b.rating FROM Movies AS m INNER JOIN BoxOffice AS b ON m.id = b.movie_id ORDER BY b.rating DESC;
+    
