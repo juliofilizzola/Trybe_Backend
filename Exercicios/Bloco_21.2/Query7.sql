0a923702178b351113fe1056a986945a3b2b274c
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 20:39:55 -0300

    atividade 5 (true)

--- Exercicios/Bloco_21.2/Query7.sql
+++ Exercicios/Bloco_21.2/Query7.sql
@@ -10,5 +10,10 @@ SELECT m.title, b.rating FROM Movies AS m INNER JOIN BoxOffice AS b ON m.id = b.
 -- atividade 4
 SELECT t.name, t.location, m.title, m.director, m.year, m.length_minutes FROM Theater t 
 	LEFT JOIN Movies AS m ON t.id = m.theater_id ORDER BY t.name;
+    
+-- atividade 5
+
+SELECT t.name, t.location, m.title, m.director, m.year, m.length_minutes FROM Theater t 
+	RIGHT JOIN Movies AS m ON t.id = m.theater_id ORDER BY t.name;
 
     
