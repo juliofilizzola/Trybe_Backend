d9a92fe7d848b2fd19f478b50aa3f22c645fed0e
Filizzola <juliofilizzola@hotmail.com>
2021-08-26 12:17:15 -0300

    atividade de fixação 1

--- Exercicios/Bloco_21.3/Query9.sql
+++ Exercicios/Bloco_21.3/Query9.sql
@@ -1,1 +1,10 @@
 USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE CountActor()
+BEGIN
+	SELECT actor_id, COUNT(*) AS 'Quantidade de filmes' FROM film_actor
+    GROUP BY actor_id ORDER BY COUNT(*) DESC LIMIT 10;
+END $$
+
+DELIMITER ;
