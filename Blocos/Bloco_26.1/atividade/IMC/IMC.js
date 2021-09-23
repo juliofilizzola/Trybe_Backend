22e5c6b688d1cf8ed4b0775e30315e19d9ebbdde
filizzola <juliofilizzola@hotmail.com>
2021-09-23 09:51:12 -0300

    atividade 1 finalizada

--- Blocos/Bloco_26.1/atividade/IMC/IMC.js
+++ Blocos/Bloco_26.1/atividade/IMC/IMC.js
@@ -4,6 +4,21 @@ const calIMC = (peso, altura) => {
   const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
 
   console.log(`IMC: ${imc}`);
+  if(imc === 18,5) {
+    console.log("Abaixo do peso (magreza)");
+  } else if (imc > 18,5 && imc < 24,9) {
+    console.log("peso normal");
+  } else if (imc > 24,9 && imc < 29,9) {
+    console.log("Acima do peso (sobrepeso)");
+  } else if (imc > 24,9 && imc < 29,9) {
+    console.log("Acima do peso (sobrepeso)");
+  } else if (imc > 30,0 && imc < 34,9) {
+    console.log("Obesidade grau I");
+  } else if (imc > 35,0 && imc < 39,9) {
+    console.log("Obesidade grau II");
+  }  else if (imc > 40,0 ) {
+    console.log("Obesidade grau III");
+  } 
 }
 // calIMC(92, 168);
 module.exports = calIMC;
