f7c035f894221873a039489a1bbeab16123dc07c
filizzola <juliofilizzola@hotmail.com>
2021-09-27 18:46:33 -0300

    aumentando complexidade da função e criando mais teste

--- Blocos/Bloco_26.3/exemplo/ModuloCalc/index.js
+++ Blocos/Bloco_26.3/exemplo/ModuloCalc/index.js
@@ -1,5 +1,12 @@
 const calculadora = (media) => {
-  return media > 7 ? "aprovado" : "Reprovado"
+  if (!typeof media === 'number') return undefined;
+
+ if (media > 0 && media <= 6 ) {
+    return "Reprovado";
+  } else if (media >= 7) {
+    return "Aprovado"
+  }
+
 }
 
 module.exports = calculadora;
--- Blocos/Bloco_26.3/exemplo/ModuloCalc/test/index.test.js
+++ Blocos/Bloco_26.3/exemplo/ModuloCalc/test/index.test.js
@@ -2,11 +2,19 @@ const { expect } = require('chai');
 
 const calcular = require('../index');
 
-const numberMedia = 8
-const RequestSuccess = "aprovado"
+const numberMedia = 8;
+const numberMediaError = 5;
+const RequestSuccess = "Aprovado";
+const RequestError = "Reprovado";
 
 describe("Testando função de calcular", () => {
+  it("verificando se retorno é undfined caso não for passando um numero", () => {
+    expect(calcular("xablau")).equals(undefined);
+  });
   it("enviando determinado valor, ele é aprovado", ()=>{
     expect(calcular(numberMedia)).equals(RequestSuccess);
+  });
+  it("enviando determinado valor, ele é Reprovado", ()=>{
+    expect(calcular(numberMediaError)).equals(RequestError);
   });
 });
