33ea329a5bd6729b8fa7f61523c52d41d71a3cf9
filizzola <juliofilizzola@hotmail.com>
2021-09-28 10:02:38 -0300

    testando se o retorno é neutro]

--- Blocos/Bloco_26.3/atividade/atividade1/test/index.test.js
+++ Blocos/Bloco_26.3/atividade/atividade1/test/index.test.js
@@ -1,11 +1,23 @@
 const { expect } = require('chai');
 const numberThis = require('../numberThis');
+const Positivo = "Positivo";
+const Neutro = "Neutro";
+const Negativo = "Negativo";
 
-describe('testando function', () => {
+describe('verificando function', () => {
   it("verificando se é uma function", () => {
     expect(numberThis).to.be.a('function');
   });
   it("verificando se o retorno é um string", () => {
     expect(numberThis(1)).to.be.a('string');
+  });
+});
+
+describe('Verificando a funcionalidade da função', () => {
+  it("testando se passardo o numero 1, o retorno vai ser Positivo", () => {
+    expect(numberThis(1)).to.be.equal(Positivo);
+  });
+  it("testando se passardo o numero 0, o retorno vai ser Neutro", () => {
+    expect(numberThis(0)).to.be.equal(Neutro);
   });
 });
