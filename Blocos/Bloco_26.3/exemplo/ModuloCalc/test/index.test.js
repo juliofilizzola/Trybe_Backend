b4b2626311f84e56d830cf1f6be8947b1c78ee2e
filizzola <juliofilizzola@hotmail.com>
2021-09-28 08:11:02 -0300

    criando mais um teste (verificando se e uma funçao)

--- Blocos/Bloco_26.3/exemplo/ModuloCalc/test/index.test.js
+++ Blocos/Bloco_26.3/exemplo/ModuloCalc/test/index.test.js
@@ -8,10 +8,13 @@ const RequestSuccess = "Aprovado";
 const RequestError = "Reprovado";
 
 describe("Testando função de calcular", () => {
+  it("Verificando se existe a função de calcular", () => {
+    expect(calcular).to.be.a('function');
+  });
   it("verificando se retorno é undfined caso não for passando um numero", () => {
     expect(calcular("xablau")).equals(undefined);
   });
-  it("enviando determinado valor, ele é Reprovado", ()=>{
+  it("Verficando se o retorno é uma string", ()=>{
     expect(calcular(numberMediaError)).to.be.a('string');
   });
   it("enviando determinado valor, ele é aprovado", ()=>{
