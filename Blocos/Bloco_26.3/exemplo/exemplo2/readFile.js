52b88248aca34f80c7f11e4fa7dfe7009146a355
filizzola <juliofilizzola@hotmail.com>
2021-09-28 08:56:27 -0300

    estrutura finalizanda

--- Blocos/Bloco_26.3/exemplo/exemplo2/readFile.js
+++ Blocos/Bloco_26.3/exemplo/exemplo2/readFile.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
 const readFile = (nameDoArquivo) => {
-  
+  try {
+    const conteudoDoArquivo = fs.readFileSync(nomeDoArquivo, 'utf8');
+
+    return conteudoDoArquivo;
+  } catch (err) {
+    return null;
+  }
 }
 
 module.exports = () => {
