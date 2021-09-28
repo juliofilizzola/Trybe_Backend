bf1e9efcd0c0a792474da20de75574e86b23f61e
filizzola <juliofilizzola@hotmail.com>
2021-09-28 09:53:55 -0300

    criando teste para retorno da função

--- Blocos/Bloco_26.3/atividade/atividade1/numberThis.js
+++ Blocos/Bloco_26.3/atividade/atividade1/numberThis.js
@@ -1,5 +1,7 @@
 const numberThis = (number) => {
-  return number;
+  if (number < 0) return "Negativo";
+  if (number === 0) return "Neutro";
+  if (number > 0) return "Positivo";
 };
 
 module.exports = numberThis;
--- Blocos/Bloco_26.3/atividade/atividade1/test/index.test.js
+++ Blocos/Bloco_26.3/atividade/atividade1/test/index.test.js
@@ -5,4 +5,7 @@ describe('testando function', () => {
   it("verificando se é uma function", () => {
     expect(numberThis).to.be.a('function');
   });
+  it("verificando se o retorno é um string", () => {
+    expect(numberThis(1)).to.be.a('string');
+  });
 });
