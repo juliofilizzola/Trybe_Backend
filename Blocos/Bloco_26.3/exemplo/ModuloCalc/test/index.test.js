543180b8b0c7f13ad4de426fd573fc9357252c52
filizzola <juliofilizzola@hotmail.com>
2021-09-28 08:02:58 -0300

    editando package.json | criando mais teste

--- Blocos/Bloco_26.3/exemplo/ModuloCalc/test/index.test.js
+++ Blocos/Bloco_26.3/exemplo/ModuloCalc/test/index.test.js
@@ -11,6 +11,9 @@ describe("Testando função de calcular", () => {
   it("verificando se retorno é undfined caso não for passando um numero", () => {
     expect(calcular("xablau")).equals(undefined);
   });
+  it("enviando determinado valor, ele é Reprovado", ()=>{
+    expect(calcular(numberMediaError)).to.be.a('string');
+  });
   it("enviando determinado valor, ele é aprovado", ()=>{
     expect(calcular(numberMedia)).equals(RequestSuccess);
   });
