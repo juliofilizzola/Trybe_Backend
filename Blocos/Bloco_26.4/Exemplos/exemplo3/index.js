d45616b38c9d824f7ec59d2e9fd3a1d66672a642
filizzola <juliofilizzola@hotmail.com>
2021-09-30 20:40:12 -0300

    instalando dependecias do body

--- Blocos/Bloco_26.4/Exemplos/exemplo3/index.js
+++ Blocos/Bloco_26.4/Exemplos/exemplo3/index.js
@@ -11,13 +11,19 @@ const drinks = [
   { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
+});
+
 app.get('/drinks', function (req, res) {
   const drink = drinks.sort((a, b) =>  a.name.localeCompare(b.name) );
   res.json(drink);
-});
-
-app.listen(3001, () => {
-  console.log('Aplicação ouvindo na porta 3001');
 });
 
 app.get('/drinks/search', function (req, res) {
@@ -35,6 +41,6 @@ app.get('/drinks/:id', function (req, res) {
   res.status(200).json(drink);
 });
 
-
-
-
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
