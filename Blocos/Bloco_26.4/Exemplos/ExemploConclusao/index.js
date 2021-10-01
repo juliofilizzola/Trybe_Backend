fb518e17c9b43baf424f0f2702ac70443606d6d0
filizzola <juliofilizzola@hotmail.com>
2021-09-30 22:17:41 -0300

    finaliznado

--- Blocos/Bloco_26.4/Exemplos/ExemploConclusao/index.js
+++ Blocos/Bloco_26.4/Exemplos/ExemploConclusao/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const recipes = [
+let recipes = [
   { id: 1, name: 'Lasanha', preco: 40.0, tempoDePreparo: 30 },
   { id: 2, name: 'Macarrão a Bolonhesa', preco: 35.0, tempoDePreparo: 25 },
   { id: 3, name: 'Macarrão com molho branco', preco: 35.0, tempoDePreparo: 25 },
@@ -36,13 +36,13 @@ app.post('/recipes', function (req, res) {
 
 app.put('/recipes/:id', function (req, res) {
   const { id } = req.params;
-  const { name, price } = req.body;
+  const { name, preco } = req.body;
   const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
 
   if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
 
-  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
-
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, preco };
+  res.json(recipes)
   res.status(204).end();
 });
 
