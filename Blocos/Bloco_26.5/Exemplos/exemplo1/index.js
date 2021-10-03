fd150063162e5be684a1805b8ca6f55c238bd386
filizzola <juliofilizzola@hotmail.com>
2021-10-03 11:25:36 -0300

    dando comando npm init e instalando express

--- Blocos/Bloco_26.5/Exemplos/exemplo1/index.js
+++ Blocos/Bloco_26.5/Exemplos/exemplo1/index.js
@@ -9,3 +9,31 @@ const recipes = [
   { id: 2, name: 'Macarrão a Bolonhesa', preco: 35.0, tempoDePreparo: 25 },
   { id: 3, name: 'Macarrão com molho branco', preco: 35.0, tempoDePreparo: 25 },
 ];
+
+// ...
+function validateName(req, res, next) {
+  const { name } = req.body;
+  if (!name || name === '') return res.status(400).json({ message: 'Invalid data!'});
+
+  next(); 
+};
+
+app.post('/recipes', validateName, function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price});
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
+app.put('/recipes/:id', validateName, function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipesIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipesIndex === -1)
+    return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipesIndex] = { ...recipes[recipesIndex], name, price };
+
+  res.status(204).end();
+});
+// ...
