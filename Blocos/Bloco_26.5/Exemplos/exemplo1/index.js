811cccec05284bd570fdccacc0478b7975d65ff7
filizzola <juliofilizzola@hotmail.com>
2021-10-03 10:52:45 -0300

    iniciando exemplo|midddlewares

---
+++ Blocos/Bloco_26.5/Exemplos/exemplo1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const recipes = [
+  { id: 1, name: 'Lasanha', preco: 40.0, tempoDePreparo: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', preco: 35.0, tempoDePreparo: 25 },
+  { id: 3, name: 'Macarrão com molho branco', preco: 35.0, tempoDePreparo: 25 },
+];
