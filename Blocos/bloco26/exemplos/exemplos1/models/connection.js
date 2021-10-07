ccffda4c92a23fc99c840fb869141155aea45714
filizzola <juliofilizzola@hotmail.com>
2021-10-07 09:59:48 -0300

    criando models

--- Blocos/bloco26/exemplos/exemplos1/models/connection.js
+++ Blocos/bloco26/exemplos/exemplos1/models/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  user: 'filizzola',
-  password: 'coisanerd1',
+  user: 'root',
+  password: '',
   host: 'localhost',
   database: 'model_example'
 });
