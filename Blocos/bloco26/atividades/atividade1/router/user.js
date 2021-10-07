656004df79e2b4859ac25b07b02a1036fe9c5c74
filizzola <juliofilizzola@hotmail.com>
2021-10-07 20:26:34 -0300

    importando isValid na rota '/user/new'

--- Blocos/bloco26/atividades/atividade1/router/user.js
+++ Blocos/bloco26/atividades/atividade1/router/user.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const { getUser, getId } = require('../middleware/middleware');
+const { isValid } = require('../middleware/validation');
+
 
 router.get('/users', getUser);
 router.get('/users/:id', getId);
+router.put('/users/new', isValid);
 
 module.exports = router;
