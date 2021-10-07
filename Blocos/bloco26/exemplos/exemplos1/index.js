4fee1172aa1b4fdbdafb8d3be867ef8f5bcb67c2
filizzola <juliofilizzola@hotmail.com>
2021-10-07 12:38:21 -0300

    finaliznado mysql

--- Blocos/bloco26/exemplos/exemplos1/index.js
+++ Blocos/bloco26/exemplos/exemplos1/index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
 
 const Author = require('./models/Author');
 
@@ -15,6 +18,18 @@ app.get('/authors/:id', async (req, res) => {
   const authors = await Author.getAuthorById(id);
   
   res.status(200).json(authors);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+      return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
 });
 
 app.listen(port, () => console.log(`Example app listening on port port!`));
