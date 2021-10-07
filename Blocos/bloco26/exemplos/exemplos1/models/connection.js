03ab2f6a931045cef929eedc56c6a5dacd2cdf89
filizzola <juliofilizzola@hotmail.com>
2021-10-07 11:02:00 -0300

    finalizando exemplo

--- Blocos/bloco26/exemplos/exemplos1/models/connection.js
+++ Blocos/bloco26/exemplos/exemplos1/models/connection.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2/promise');
+require('dotenv/config');
 
 const connection = mysql.createPool({
-  user: '',
-  password: '',
+  user: process.env.ROOT,
+  password: process.env.PASSWORD,
   host: 'localhost',
   database: 'model_example'
 });
