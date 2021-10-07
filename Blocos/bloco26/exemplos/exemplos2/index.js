f36250f16907ab5291764056b31e4e0768caaf99
filizzola <juliofilizzola@hotmail.com>
2021-10-07 14:42:10 -0300

    modificando package.json

---
+++ Blocos/bloco26/exemplos/exemplos2/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
+
+const Author = require('./models/Author');
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const authors = await Author.getAuthorById(id);
+  
+  res.status(200).json(authors);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+      return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
+});
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
