0bb975cc3da74c6dc763bf9fe2174dae28752f85
filizzola <juliofilizzola@hotmail.com>
2021-10-09 09:09:24 -0300

    refatorando código e comentando

--- Blocos/bloco26/atividades/atividade1/models/connection.js
+++ Blocos/bloco26/atividades/atividade1/models/connection.js
@@ -10,13 +10,13 @@ const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
 let db = null;
 
 const connection = () => {
-  return MongoClient
-    .connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) =>  conn.db('users'))
-    .catch((error) => {
-      console.error(error);
-      process.exit();
-    })
-};
+  return db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => {
+  db = conn.db('model_example');
+  return db;
+  })
+}; // sigleton (usar no projeto)
 
 module.exports = connection; 
