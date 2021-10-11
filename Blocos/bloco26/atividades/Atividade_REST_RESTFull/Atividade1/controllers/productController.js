63b7e875851c6cf47b2fcc10a4b9ca1af8e92a2e
filizzola <juliofilizzola@hotmail.com>
2021-10-11 15:59:40 -0300

    criando funções de controles

---
+++ Blocos/bloco26/atividades/Atividade_REST_RESTFull/Atividade1/controllers/productController.js
@@ -0,0 +1,44 @@
+const ProductModel = require('../models/productModel');
+
+const getAllProducts = async (req, res) => {
+  const products = await ProductModel.getAll();
+
+  res.send(products);
+};
+
+const getProductID = async (req, res) => {
+  const { id } = req.params;
+  const products = await ProductModel.getById(id);
+
+  res.send(products);
+};
+
+const addNewProduct = async (req, res) => {
+  const { name, brand } = req.body;
+
+  const newProduct = await ProductModel.add(name, brand);
+
+  res.send(newProduct);
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  const products = await ProductModel.exclude(id);
+  res.send(products);
+};
+
+const updateProduct = async (req, res) => {
+  const { name, brand } = req.body;
+  const { id } = req.params;
+  const products = await ProductModel.update(id, name, brand);
+
+  res.send(products);
+};
+
+module.exports = {
+  getAllProducts,
+  getProductID,
+  addNewProduct,
+  deleteProduct,
+  updateProduct
+}
