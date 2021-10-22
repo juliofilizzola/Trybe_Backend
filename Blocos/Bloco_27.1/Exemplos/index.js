2b853155ab85376670663bc01f3f16aedf3c8067
filizzola <juliofilizzola@hotmail.com>
2021-10-22 09:32:47 -0300

    importando router no index.js

--- Blocos/Bloco_27.1/Exemplos/index.js
+++ Blocos/Bloco_27.1/Exemplos/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const router = require('./router/router');
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/', router);
 
 app.listen(PORT, () => console.log(`listen ON port: ${PORT}`));
